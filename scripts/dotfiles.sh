076c4c9bd7eeeb26cee4790e8edb76a281d49272
mario <goffredo2004@gmail.com>
2023-03-26 18:27:03 +0200

    made new rofi script for opening books

---
+++ scripts/dotfiles.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+
+loc="~/bench/dotfiles"
+
+cp -f ~/.config/hypr/hyprland.conf ~/bench/dotfiles/hypr/
+cp -f ~/.config/nvim/init.lua ~/bench/dotfiles/nvim/
+cp -rf ~/.config/nvim/lua ~/bench/dotfiles/nvim/
+cp -f ~/.config/kitty/kitty.conf ~/bench/dotfiles/kitty/
+cp -rf ~/.config/rofi/ ~/bench/dotfiles/
+cp -rf ~/.config/waybar/ ~/bench/dotfiles/
+cp -f ~/.config/zsh/zshenv ~/bench/dotfiles/zsh/
+cp -f ~/.config/zsh/.zshrc ~/bench/dotfiles/zsh/
+cp -f ~/.config/zsh/aliases ~/bench/dotfiles/zsh/
+cp -f ~/.config/zsh/environment ~/bench/dotfiles/zsh/
+cp -f ~/.config/zsh/fzf.zsh ~/bench/dotfiles/zsh/
+cp -f ~/.config/zathura/zathurarc ~/bench/dotfiles/zathura/
+cp -f ~/.config/zathura/zathurarc ~/bench/dotfiles/zathura/
+cp -rf ~/.config/newsboat/ ~/bench/dotfiles/
+cp -f ~/.config/ncmpcpp/config ~/bench/dotfiles/ncmpcpp/
+cp -f ~/.config/ncmpcpp/bindings ~/bench/dotfiles/ncmpcpp/
+cp -rf ~/.config/lf/ ~/bench/dotfiles/
+cp -rf ~/.config/imv/ ~/bench/dotfiles/
+cp -f ~/.config/glava/bars.glsl ~/bench/dotfiles/glava/
+cp -rf ~/.mozilla/mario/chrome/* ~/bench/dotfiles/firefox
+cp -r ~/.local/bin/dotfiles.sh ~/bench/dotfiles/scripts
+cp -r ~/.local/bin/rofi-library.sh ~/bench/dotfiles/scripts
+cp -r ~/.local/bin/rofi-power.sh ~/bench/dotfiles/scripts
---
+++ scripts/rofi-library.sh
@@ -0,0 +1,79 @@
+
+# The commented script is slow so I am rewriting it
+
+library="$HOME/vault/library" # There are only pdfs, epubs, and djvus in here
+cd $library
+theme="-theme ~/.config/rofi/themes/library.rasi"
+
+xdg-open "$library/$( \
+  fd . -tf -e 'pdf' -e 'epub' -e 'djvu' | \
+  rofi -dmenu -i -matching fuzzy -no-custom -location 0 -p "" $theme \
+  )"
+
+# #!/usr/bin/env bash
+#
+# # -----------------------------------------------------------------------------
+# # Info:
+# #   author:    Miroslav Vidovic
+# #   file:      books-search.sh
+# #   created:   13.08.2017.-08:06:54
+# #   revision:  ---
+# #   version:   1.0
+# # -----------------------------------------------------------------------------
+# # Requirements:
+# #   rofi
+# # Description:
+# #   Use rofi to search my books.
+# # Usage:
+# #   books-search.sh
+# # -----------------------------------------------------------------------------
+# # Script:
+#
+# # Books directory
+# BOOKS_DIR=$HOME/vault/library/
+#
+# # Save find result to F_ARRAY
+# readarray -t F_ARRAY <<< "$(/usr/bin/fd . "$BOOKS_DIR" -tf -e 'pdf' -e 'epub' -e 'djvu')"
+#
+# # Associative array for storing books
+# # key => book name
+# # value => absolute path to the file
+# # BOOKS['filename']='path'
+# declare -A BOOKS
+#
+# # Add elements to BOOKS array
+# get_books() {
+#
+#   # if [ ${#F_ARRAY[@]} != 0 ]; then
+#   if [[ ! -z ${F_ARRAY[@]} ]]; then
+#     for i in "${!F_ARRAY[@]}"
+#     do
+#       path=${F_ARRAY[$i]}
+#       file=$(basename "${F_ARRAY[$i]}")
+#       BOOKS+=(["$file"]="$path")
+#     done
+#   else
+#       echo "I didn't find any book in $BOOKS_DIR is empty!"
+#   fi
+# }
+#
+# # List for rofi
+# gen_list(){
+#   for i in "${!BOOKS[@]}"
+#   do
+#     echo "$i"
+#   done
+# }
+#
+# main() {
+#   get_books
+#   book=$( (gen_list) | rofi -dmenu -i -matching fuzzy -no-custom -location 0 -p "" -theme ~/.config/rofi/themes/library.rasi )
+#
+#   if [ -n "$book" ]; then
+#     xdg-open "${BOOKS[$book]}"
+#   fi
+# }
+#
+# main
+#
+# exit 0
