aa3a91bd3e4fa0d8c4000ebaddd8d7b817e2fbe7
mario <goffredo2004@gmail.com>
2023-03-29 12:52:32 +0200

    removed history from rofi-calc

--- scripts/rofi-library.sh
+++ scripts/rofi-library.sh
@@ -1,14 +1,17 @@
-
+#!/bin/sh
 # The commented script is slow so I am rewriting it
 
 library="$HOME/vault/library" # There are only pdfs, epubs, and djvus in here
 cd $library
 theme="-theme ~/.config/rofi/themes/library.rasi"
 
-xdg-open "$library/$( \
-  fd . -tf -e 'pdf' -e 'epub' -e 'djvu' | \
-  rofi -dmenu -i -matching fuzzy -no-custom -location 0 -p "" $theme \
-  )"
+file=$( fd . -tf -e 'pdf' -e 'epub' -e 'djvu' | rofi -dmenu -i -matching fuzzy -no-custom -location 0 -p "" $theme )
+
+if [ "$file" ] ; then
+  zathura $library/"$file"
+  exit 0
+fi
+exit 1
 
 # #!/usr/bin/env bash
 #
