0740d7e288e87ee3c91db2bd7d568367f6fcf31a
mario <goffredo2004@gmail.com>
2023-04-06 20:10:57 +0200

    added Neovim configuration files

---
+++ nvim/lua/plugins/indent-blankline.lua
@@ -0,0 +1,21 @@
+return {
+  "lukas-reineke/indent-blankline.nvim",
+  config = function ()
+    require("indent_blankline").setup {
+      char = '│',
+      space_char_blankline = " ",
+      show_trailing_blankline_indent = false,
+      -- Context indent highlighted by treesitter
+      use_tresitter = false,
+      show_current_context = true,
+      show_current_context_start = true,
+      filetype_exclude = {
+        "toggleterm",
+        "help",
+        "man",
+        "text",
+        "asm",
+      }
+    }
+  end
+}
---
+++ nvim/lua/plugins/nvim-autopairs.lua
@@ -0,0 +1,7 @@
+return {
+  "windwp/nvim-autopairs",
+  event = { "BufReadPost", "BufNewFile" },
+  config = function()
+    require("nvim-autopairs").setup {}
+  end
+}
