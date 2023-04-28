66f62dae785f465604468c4f39f02707b0c5fe04
mario <goffredo2004@gmail.com>
2023-04-28 09:37:19 +0200

    made a little correction

--- dotfiles/scripts/rofi-library.sh
+++ dotfiles/scripts/rofi-library.sh
@@ -1,23 +1,23 @@
 #!/bin/bash
-# The commented script is slow so I am rewriting it
 
 library="$HOME/vault/library/books" # There are only pdfs, epubs, and djvus in here
 cd $library
-theme="-theme ~/.config/rofi/themes/library.rasi"
+rofi_theme="-theme ~/.config/rofi/themes/library.rasi"
 
 file=$(
     fd . -tf -e 'pdf' -e 'epub' -e 'djvu' | \
-    rofi -dmenu -i -matching normal -p "" $theme \
+    rofi -dmenu -i -matching normal -p "" $rofi_theme \
 )
 
 if [[ $file ]]; then
     if [[ $file == *.epub ]]; then
         # Terminal ebook reader
-         # https://github.com/wustho/epy
-         $TERM epy $library/"$file"
-         exit 0
-        else
-          zathura $library/"$file"
-        fi
+        # https://github.com/wustho/epy
+        $TERM epy $library/"$file"
+        exit 0
+    else
+        zathura $library/"$file"
+        exit 0
+    fi
 fi
 exit 1
