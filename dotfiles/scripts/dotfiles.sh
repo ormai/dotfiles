0a8c1fbaa55d5c7605edfbf3fffe8dc681bd13c4
mario <goffredo2004@gmail.com>
2023-04-28 09:33:56 +0200

    restrucutred the file tree

---
+++ dotfiles/scripts/dotfiles.sh
@@ -0,0 +1,31 @@
+#!/bin/sh
+
+# If you are wondering were this files come from:
+
+cp -f ~/.config/hypr/hyprland.conf ~/bench/dotfiles/dotfiles/hypr/
+cp -f ~/.config/nvim/init.lua ~/bench/dotfiles/dotfiles/nvim/
+cp -rf ~/.config/nvim/lua ~/bench/dotfiles/dotfiles/nvim/
+cp -f ~/.config/kitty/kitty.conf ~/bench/dotfiles/dotfiles/kitty/
+cp -rf ~/.config/rofi/ ~/bench/dotfiles/dotfiles/
+cp -rf ~/.config/waybar/ ~/bench/dotfiles/dotfiles/
+cp -f ~/.config/zsh/.zshrc ~/bench/dotfiles/dotfiles/zsh/
+cp -f ~/.config/zsh/aliases.zsh ~/bench/dotfiles/dotfiles/zsh/
+cp -f ~/.config/zsh/environment.zsh ~/bench/dotfiles/dotfiles/zsh/
+cp -f ~/.config/zsh/fzf.zsh ~/bench/dotfiles/dotfiles/zsh/
+cp -f ~/.config/zathura/zathurarc ~/bench/dotfiles/dotfiles/zathura/
+cp -f ~/.config/zathura/zathurarc ~/bench/dotfiles/dotfiles/zathura/
+cp -rf ~/.config/newsboat/ ~/bench/dotfiles/dotfiles/
+cp -f ~/.config/ncmpcpp/config ~/bench/dotfiles/dotfiles/ncmpcpp/
+cp -f ~/.config/ncmpcpp/bindings ~/bench/dotfiles/dotfiles/ncmpcpp/
+cp -rf ~/.config/lf/ ~/bench/dotfiles/dotfiles/
+cp -rf ~/.config/imv/ ~/bench/dotfiles/dotfiles/
+cp -f ~/.config/glava/bars.glsl ~/bench/dotfiles/dotfiles/glava/
+cp -rf ~/.mozilla/mario/chrome/* ~/bench/dotfiles/dotfiles/firefox/
+cp -f ~/.local/bin/dotfiles.sh ~/bench/dotfiles/dotfiles/scripts/
+cp -f ~/.local/bin/rofi-library.sh ~/bench/dotfiles/dotfiles/scripts/
+cp -f ~/.local/bin/rofi-power.sh ~/bench/dotfiles/dotfiles/scripts/
+cp -f ~/.local/bin/hyprland_focus-mode.sh ~/bench/dotfiles/dotfiles/scripts/
+cp -f ~/.local/bin/lock.sh ~/bench/dotfiles/dotfiles/scripts/
+cp -f ~/.local/bin/idle.sh ~/bench/dotfiles/dotfiles/scripts/
+cp -f ~/.local/bin/waybar-weather-indicator.sh ~/bench/dotfiles/dotfiles/scripts/
+cp -f ~/.local/bin/panes.sh ~/bench/dotfiles/dotfiles/scripts/
