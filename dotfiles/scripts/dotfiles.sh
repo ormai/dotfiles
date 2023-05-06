6958ff1c056b2f6a53bdf573b6487fc8a191d7ab
mario <goffredo2004@gmail.com>
2023-05-06 21:14:06 +0200

    it sounds bad to force things

--- dotfiles/scripts/dotfiles.sh
+++ dotfiles/scripts/dotfiles.sh
@@ -2,31 +2,39 @@
 
 # If you were wondering were this files come from
 
-cp -f ~/.config/hypr/hyprland.conf ~/bench/dotfiles/dotfiles/hypr/
-cp -f ~/.config/nvim/init.lua ~/bench/dotfiles/dotfiles/nvim/
-cp -rf ~/.config/nvim/lua ~/bench/dotfiles/dotfiles/nvim/
-cp -f ~/.config/kitty/kitty.conf ~/bench/dotfiles/dotfiles/kitty/
-cp -f ~/.config/kitty/gruvbox_dark.conf ~/bench/dotfiles/dotfiles/kitty/
-cp -rf ~/.config/rofi/ ~/bench/dotfiles/dotfiles/
-cp -rf ~/.config/waybar/ ~/bench/dotfiles/dotfiles/
-cp -f ~/.config/zsh/.zshrc ~/bench/dotfiles/dotfiles/zsh/
-cp -f ~/.config/zsh/aliases.zsh ~/bench/dotfiles/dotfiles/zsh/
-cp -f ~/.config/zsh/environment.zsh ~/bench/dotfiles/dotfiles/zsh/
-cp -f ~/.config/zsh/fzf.zsh ~/bench/dotfiles/dotfiles/zsh/
-cp -f ~/.config/zathura/zathurarc ~/bench/dotfiles/dotfiles/zathura/
-cp -f ~/.config/zathura/zathurarc ~/bench/dotfiles/dotfiles/zathura/
-cp -rf ~/.config/newsboat/ ~/bench/dotfiles/dotfiles/
-cp -f ~/.config/ncmpcpp/config ~/bench/dotfiles/dotfiles/ncmpcpp/
-cp -f ~/.config/ncmpcpp/bindings ~/bench/dotfiles/dotfiles/ncmpcpp/
-cp -rf ~/.config/lf/ ~/bench/dotfiles/dotfiles/
-cp -rf ~/.config/imv/ ~/bench/dotfiles/dotfiles/
-cp -f ~/.config/glava/bars.glsl ~/bench/dotfiles/dotfiles/glava/
-cp -rf ~/.mozilla/mario/chrome/* ~/bench/dotfiles/dotfiles/firefox/
-cp -f ~/.local/bin/dotfiles.sh ~/bench/dotfiles/dotfiles/scripts/
-cp -f ~/.local/bin/rofi-library.sh ~/bench/dotfiles/dotfiles/scripts/
-cp -f ~/.local/bin/rofi-power.sh ~/bench/dotfiles/dotfiles/scripts/
-cp -f ~/.local/bin/hyprland_focus-mode.sh ~/bench/dotfiles/dotfiles/scripts/
-cp -f ~/.local/bin/lock.sh ~/bench/dotfiles/dotfiles/scripts/
-cp -f ~/.local/bin/idle.sh ~/bench/dotfiles/dotfiles/scripts/
-cp -f ~/.local/bin/waybar-weather-indicator.sh ~/bench/dotfiles/dotfiles/scripts/
-cp -f ~/.local/bin/panes.sh ~/bench/dotfiles/dotfiles/scripts/
+\cp ~/.config/hypr/hyprland.conf ~/bench/dotfiles/dotfiles/hypr/
+\cp -r ~/.config/waybar/ ~/bench/dotfiles/dotfiles/
+
+\cp ~/.config/nvim/init.lua ~/bench/dotfiles/dotfiles/nvim/
+\cp -r ~/.config/nvim/lua ~/bench/dotfiles/dotfiles/nvim/
+
+\cp ~/.config/kitty/kitty.conf ~/bench/dotfiles/dotfiles/kitty/
+\cp ~/.config/kitty/gruvbox_dark.conf ~/bench/dotfiles/dotfiles/kitty/
+
+\cp -r ~/.config/rofi/ ~/bench/dotfiles/dotfiles/
+
+\cp ~/.config/zsh/.zshrc ~/bench/dotfiles/dotfiles/zsh/
+\cp ~/.config/zsh/aliases.zsh ~/bench/dotfiles/dotfiles/zsh/
+\cp ~/.config/zsh/environment.zsh ~/bench/dotfiles/dotfiles/zsh/
+\cp ~/.config/zsh/fzf.zsh ~/bench/dotfiles/dotfiles/zsh/
+
+\cp ~/.config/zathura/zathurarc ~/bench/dotfiles/dotfiles/zathura/
+\cp ~/.config/zathura/zathurarc ~/bench/dotfiles/dotfiles/zathura/
+\cp -r ~/.config/newsboat/ ~/bench/dotfiles/dotfiles/
+\cp ~/.config/ncmpcpp/config ~/bench/dotfiles/dotfiles/ncmpcpp/
+\cp ~/.config/ncmpcpp/bindings ~/bench/dotfiles/dotfiles/ncmpcpp/
+
+\cp -r ~/.config/lf/ ~/bench/dotfiles/dotfiles/
+\cp -r ~/.config/imv/ ~/bench/dotfiles/dotfiles/
+\cp ~/.config/glava/bars.glsl ~/bench/dotfiles/dotfiles/glava/
+
+\cp -r ~/.mozilla/mario/chrome/* ~/bench/dotfiles/dotfiles/firefox/
+
+\cp ~/.local/bin/dotfiles.sh ~/bench/dotfiles/dotfiles/scripts/
+\cp ~/.local/bin/rofi-library.sh ~/bench/dotfiles/dotfiles/scripts/
+\cp ~/.local/bin/rofi-power.sh ~/bench/dotfiles/dotfiles/scripts/
+\cp ~/.local/bin/hyprland_focus-mode.sh ~/bench/dotfiles/dotfiles/scripts/
+\cp ~/.local/bin/lock.sh ~/bench/dotfiles/dotfiles/scripts/
+\cp ~/.local/bin/idle.sh ~/bench/dotfiles/dotfiles/scripts/
+\cp ~/.local/bin/waybar-weather-indicator.sh ~/bench/dotfiles/dotfiles/scripts/
+\cp ~/.local/bin/panes.sh ~/bench/dotfiles/dotfiles/scripts/
