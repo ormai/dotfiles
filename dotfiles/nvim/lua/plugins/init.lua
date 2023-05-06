a2d19d10abddf54eb5ff53c79a47b9481e00302b
mario <goffredo2004@gmail.com>
2023-05-06 21:13:30 +0200

    lazy load kitty syntax plugin

--- dotfiles/nvim/lua/plugins/init.lua
+++ dotfiles/nvim/lua/plugins/init.lua
@@ -2,5 +2,8 @@ return {
     "tpope/vim-fugitive",
     "tpope/vim-sleuth",
     "tpope/vim-rhubarb",
-    "fladson/vim-kitty", -- provides syntax highlighting for kitty.conf
+    {
+        "fladson/vim-kitty", -- provides syntax highlighting for kitty.conf
+        ft = "kitty",
+    },
 }
