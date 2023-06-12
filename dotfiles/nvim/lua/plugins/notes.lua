64136de44eb0df83fe3ff8d56b991015207a97ec
mario <goffredo2004@gmail.com>
2023-06-12 07:21:03 +0200

    swapped telekasten.nvim with obsidian.nvim

--- dotfiles/nvim/lua/plugins/notes.lua
+++ dotfiles/nvim/lua/plugins/notes.lua
@@ -1,21 +1,38 @@
 return {
-    {
-        "renerocksai/telekasten.nvim",
-        ft = "markdown",
-        keys = { {"<C-p>", "<cmd>Telekasten<CR>"}, },
-        dependencies = {"nvim-telescope/telescope.nvim"},
-        config = function ()
-            require('telekasten').setup({
-                home = vim.fn.expand("~/vault"), -- Put the name of your notes directory here
-            })
-        end
-    },
+
     {
         "iamcco/markdown-preview.nvim",
         ft = "markdown",
         config = function ()
             vim.cmd[[let g:mkdp_auto_start = 1]]
             vim.cmd[[let g:mkdp_markdown_css = "~/.config/nvim/markdown.css"]]
+        end
+    },
+
+    {
+        "epwalsh/obsidian.nvim",
+        event = { "BufreadPre " .. vim.fn.expand "~" .. "/vault/**.md" },
+        dependencies = {
+            "nvim-lua/plenary.nvim",
+            "hrsh7th/nvim-cmp",
+            "nvim-telescope/telescope.nvim",
+        },
+        config = function()
+            require("obsidian").setup({
+                dir = "~/vault",
+                completion = {
+                    nvim_cmp = true,
+                },
+                templates = {
+                    subdir = "meta/syntax/templates",
+                    date_format = "%d-%m-%Y",
+                    time_format = "%H_%M",
+                },
+                follow_url_func = function(url) -- open url in default browser
+                    vim.fn.jobstart({"xdg-open"}, url)
+                end,
+                finder = "telescope.nvim",
+            })
         end
     }
 }
