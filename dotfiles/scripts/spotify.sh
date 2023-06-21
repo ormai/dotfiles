28a81adf6781532f83e73ffed62cc22e4c35e0d3
ormai <goffredo2004@gmail.com>
2023-06-21 12:11:07 +0200

    new global user scripts structure

---
+++ dotfiles/scripts/spotify.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+function get_artist () {
+  echo "$(playerctl -p spotify metadata --format "{{ artist }}"| sed -e "s/[[(]....*[])]*//g" | sed "s/ *$//"| sed "s/^\(.\{20\}[^ ]*\)\(.*\)$/\1[\2]/"| sed "s/\[ *\]$//"| sed "s/\[.*\]$/.../")"
+} #
+
+function get_title () {
+  echo "$(playerctl -p spotify metadata --format "{{title}}"|sed 's/'\'\''/'\"'/g' )"
+} #
+
+[ ! -z "$(playerctl -p spotify status 2>/dev/null)" ] \
+  && artist=$(get_artist) \
+  && title=$(get_title) \
+  && (echo "$artist -" "$title")	|| exit 1
