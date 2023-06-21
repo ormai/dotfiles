3074ede259d1b13053f40e34063a51e334dc2f5d
ormai <goffredo2004@gmail.com>
2023-06-21 12:11:07 +0200

    new global user scripts structure

---
+++ dotfiles/scripts/setup.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+#
+# This is a script to set up my system. For now it's just a work in progress.
+
+# install programs
+pacman -Syu firefox
+
+# Set up NewTab in Firefox
+ln -s $HOME/.mozilla/mario/external/autoconfig.cfg /usr/lib/firefox/autoconfig.cfg
+ln -s $HOME/.mozilla/mario/external/autoconfig.js /usr/lib/firefox/defaults/pref/autoconfig.js
