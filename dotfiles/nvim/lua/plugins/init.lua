3da646572c7f9fcdb9b8432c97335ae69228c778
ormai <goffredo2004@gmail.com>
2023-08-06 10:46:26 +0200

    minor changes

--- dotfiles/nvim/lua/plugins/init.lua
+++ dotfiles/nvim/lua/plugins/init.lua
@@ -3,6 +3,10 @@ return {
     "tpope/vim-sleuth",
     "tpope/vim-rhubarb",
     {
+        "elkowar/yuck.vim",
+        ft = "yuck"
+    },
+    {
         "fladson/vim-kitty", -- provides syntax highlighting for kitty.conf
         ft = "kitty",
     },
