210e742c16035cb6eb71f05d4b40fa6ab012bc14
ormai <mariodandrea@mailfence.com>
2023-09-30 19:23:32 +0200

    scripts

---
+++ .local/bin/toggle-kitty-padding.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+# This is a wrapper scripts called through aliases whenever a tui program is
+# run by kitty.
+# Normally kitty is configured to have some `window_width_padding`, but when
+# running some program like nvim such padding should disappear.
+
+# Solution found here <https://www.reddit.com/r/vim/comments/ofe72k/disabling_terminal_padding_while_using_vim/>
+
+kitty @ set-spacing padding=0
+$@ # like a sandwich
+kitty @ set-spacing padding=5
