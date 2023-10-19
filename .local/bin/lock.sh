dfbca2812ffa83dd4e6c9101c33c1f2cefc8b143
ormai <mariodandrea@mailfence.com>
2023-10-19 10:30:18 +0200

    Routine updates

--- .local/bin/lock.sh
+++ .local/bin/lock.sh
@@ -4,8 +4,8 @@ TRANSPARENT=00000000
 BG="282828"
 BG2="3C3836"
 RING="3EC07C"
-TEXT="EBDBB2"
-ACCENT="D65D0E"
+TEXT="1d2021"
+ACCENT="B8BB26"
 RED="FB4934"
 YELLOW="FABD2F"
 GREEN="B8BB26"
@@ -16,23 +16,23 @@ RANDOM_WP=$( \ls ~/Pictures/wp/light | sort -R | tail -1 )
   --image ~/Pictures/wp/light/$RANDOM_WP \
   --ignore-empty-password \
   --show-failed-attempts \
-  --font "Inconsolata" \
-  --font-size 30 \
+  --font "Inter" \
+  --font-size 20 \
   --indicator-radius 200 \
   --indicator-thickness 10 \
   --inside-color $TRANSPARENT \
-  --inside-clear-color $BG \
-  --inside-ver-color $BG \
-  --inside-wrong-color $BG \
-  --ring-color $RING \
-  --ring-clear-color $YELLOW \
-  --ring-ver-color $GREEN \
-  --ring-wrong-color $RED \
-  --line-color $BG2 \
-  --line-clear-color $BG2 \
-  --line-wrong-color $BG2 \
-  --line-ver-color $BG2 \
-  --separator-color $BG2 \
+  --inside-clear-color $TRANSPARENT \
+  --inside-ver-color $TRANSPARENT \
+  --inside-wrong-color $TRANSPARENT \
+  --ring-color $TRANSPARENT \
+  --ring-clear-color $TRANSPARENT \
+  --ring-ver-color $TRANSPARENT \
+  --ring-wrong-color $TRANSPARENT \
+  --line-color $TRANSPARENT \
+  --line-clear-color $TRANSPARENT \
+  --line-wrong-color $TRANSPARENT \
+  --line-ver-color $TRANSPARENT \
+  --separator-color $TRANSPARENT \
   --key-hl-color $ACCENT \
   --text-color $TEXT \
   --text-clear-color $TEXT \
