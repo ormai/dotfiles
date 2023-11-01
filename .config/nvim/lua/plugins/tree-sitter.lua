bc9ccff6b66b95afe55ab1035b56fffdc6e46236
ormai <mariodandrea@mailfence.com>
2023-11-01 21:13:26 +0100

    Set up indent-blankline.nvim

--- .config/nvim/lua/plugins/tree-sitter.lua
+++ .config/nvim/lua/plugins/tree-sitter.lua
@@ -1,81 +1,43 @@
-local filetypes = {
-  -- 'rust',
-  'lua',
-  'bash',
-  'python',
-  'c',
-  'cpp',
-  'html',
-  'css',
-  'rasi',
-  'javascript',
-  'typescript',
-  'jsonc',
-  'markdown',
-  'markdown_inline'
-}
-
+---@diagnostic disable: missing-fields
 return {
   'nvim-treesitter/nvim-treesitter',
-  ft = filetypes,
+  lazy = false,
   build = ':TSUpdate',
   dependencies = {
     'p00f/nvim-ts-rainbow',
     'nvim-treesitter/nvim-treesitter-textobjects',
-    'lukas-reineke/indent-blankline.nvim',
-    'nvim-treesitter/nvim-treesitter',
   },
   keys = {
-    { '[d', vim.diagnostic.goto_prev },
-    { ']d', vim.diagnostic.goto_next },
+    { '[d',        vim.diagnostic.goto_prev },
+    { ']d',        vim.diagnostic.goto_next },
     { '<leader>e', vim.diagnostic.open_float },
     { '<leader>q', vim.diagnostic.setloclist },
     { '<c-space>', desc = 'Incremental selection' },
-    { '<bs>', desc = 'Decremental selection '}
+    { '<bs>',      desc = 'Decremental selection ' }
   },
-  config = function ()
+  config = function()
     require('nvim-treesitter.configs').setup {
-      ensure_installed = filetypes,
       auto_install = true,
-      highlight = {
-        enable = true
-      },
-      indent = {
-        enable = true
-      },
+      highlight = { enable = true },
+      indent = { enable = true },
       incremental_selection = {
         enable = true,
         keymaps = {
           init_selection = '<C-space>',
           node_incremental = '<C-space>',
-          node_decremental = '<bs>',
-        },
+          node_decremental = '<bs>'
+        }
       },
       textobjects = {
         enable = true,
-        swap = {
+        swap = { -- Swap node under the cursor
           enable = true,
-          swap_next = {
-            ['<leader>a'] = '@parameter.inner',
-          },
-          swap_previous = {
-            ['<leader>A'] = '@parameter.inner',
-          },
+          swap_next = { ['<leader>a'] = '@parameter.inner' },
+          swap_previous = { ['<leader>A'] = '@parameter.inner' }
         },
-        select = {
+        move = { -- Jump between textobjects
           enable = true,
-          lookahead = true, -- Automatically jump forward to textobj, similar to targets.vim
-          keymaps = {
-            -- You can use the capture groups defined in textobjects.scm
-            ['af'] = '@function.outer',
-            ['if'] = '@function.inner',
-            ['ac'] = '@class.outer',
-            ['ic'] = '@class.inner',
-          },
-        },
-        move = {
-          enable = true,
-          set_jumps = true, -- whether to set jumps in the jumplist
+          set_jumps = true, -- Register jumps in the jump list
           goto_next_start = {
             [']m'] = '@function.outer',
             [']]'] = '@class.outer',
@@ -91,25 +53,24 @@ return {
           goto_previous_end = {
             ['[M'] = '@function.outer',
             ['[]'] = '@class.outer',
-          },
-        },
+          }
+        }
       },
 
-      -- ## nvim-ts-rainbow
-      -- parenthesis, brackets and braces have a diffrent color depending op
+      -- parenthesis, brackets and braces have a different color depending op
       -- the nesting level
       rainbow = {
         enable = true,
-        extended_mode = true, -- Also highlight non-bracket delimiters like html tags, boolean or table: lang -> boolean
+        extended_mode = true, -- Also highlight html tags
         colors = {
-          '#83A598', -- Level 1
+          '#83A598',
           '#8ec07c',
           '#b8bb26',
           '#fabd2f',
           '#f38019',
-          '#fb4934', -- Level 6
-        },
-      },
+          '#fb4934'
+        }
+      }
     }
   end
 }
