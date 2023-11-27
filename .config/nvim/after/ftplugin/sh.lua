e547a3e76579884b26d5f44972271a6c818255e0
ormai <mariodandrea@mailfence.com>
2023-11-27 17:45:18 +0100

    Adjustment and simplification

---
+++ .config/nvim/after/ftplugin/sh.lua
@@ -0,0 +1,5 @@
+vim.keymap.set(
+  'n',
+  '<leader>r',
+  ":TermExec cmd='sh %:.'<CR>"
+)
--- .config/nvim/lua/plugins/init.lua
+++ .config/nvim/lua/plugins/init.lua
@@ -1,15 +1,15 @@
 return {
-  {
+  --[[ {
     'tpope/vim-fugitive', -- A Git wrapper so awesome, it should be illegal
     cmd = {
       'G', 'Git', 'Ggrep', 'Glrep', 'Gclog', 'Gcd', 'Glcd', 'Gedit', 'Gsplit',
       'Gvsplit', 'Gtabedit', 'Gpedit', 'Gdrop', 'Gread', 'Gwrite', 'Gdiffsplit',
       'Gvdiffsplit', 'Ghdiffsplit', 'GMove', 'Gremove', 'GUnlink', 'GBrowser'
     }
-  },
-  'tpope/vim-rhubarb', -- GitHub extension for fugitive.vim
-  'tpope/vim-sleuth',  -- Heuristically set buffer options
-  'romainl/vim-cool',  -- A very simple plugin that makes hlsearch more useful.
+  }, ]]
+  -- 'tpope/vim-rhubarb', -- GitHub extension for fugitive.vim
+  -- 'tpope/vim-sleuth',  -- Heuristically set buffer options
+  'romainl/vim-cool',    -- A very simple plugin that makes hlsearch more useful.
   {
     'folke/neodev.nvim', -- Neovim setup for init.lua and plugin development...
     ft = 'lua',
@@ -37,5 +37,14 @@ return {
         enabled = false
       }
     }
-  }
+  },
+  {
+    'Beastwick18/nabla.nvim', -- tmp
+    ft = { 'markdown', 'tex' },
+    config = function()
+      require('nabla').enable_virt({
+        align_center = false
+      });
+    end
+  },
 }
