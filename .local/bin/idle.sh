c83e25cfd4868312613cd5f9f0b58465952a6446
Mario D'Andrea <goffredo2004@gmail.com>
2024-02-01 14:50:02 +0100

    Lint

--- .local/bin/idle.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-/usr/bin/swayidle -w \
-  timeout 180 lock.sh \
-  timeout 200 "swaymsg 'output \'*\' power off'" \
-  resume "swaymsg 'output \'*\' power on'" \
-  before-sleep lock.sh
