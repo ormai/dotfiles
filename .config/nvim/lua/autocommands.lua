6263974bd9d0966e96ba2861009d2c2617e28e62
Mario D'Andrea <goffredo2004@gmail.com>
2024-02-22 17:57:08 +0100

    Replace auto-save.nvim with rudimentary autocmd

--- .config/nvim/lua/autocommands.lua
+++ .config/nvim/lua/autocommands.lua
@@ -1,16 +1,28 @@
--- :help vim.highlight.on_yank()
+-- :h vim.highlight.on_yank()
 vim.api.nvim_create_autocmd('TextYankPost', {
   desc = "Highlight on yank",
-	callback = function() vim.highlight.on_yank() end,
-	group = vim.api.nvim_create_augroup('YankHighlight', { clear = true }),
-	pattern = '*',
+  callback = function() vim.highlight.on_yank() end,
+  group = vim.api.nvim_create_augroup('YankHighlight', { clear = true }),
+  pattern = '*',
 })
 
 -- https://www.reddit.com/r/neovim/comments/1abd2cq/comment/kjo7moz/
--- :help last-position-jump
+-- :h last-position-jump
 vim.api.nvim_create_autocmd('BufReadPost', {
   desc = 'Open file at last edit position',
   group = misc_augroup,
   pattern = '*',
   command = 'silent! normal! g`"zv'
 })
+
+-- https://github.com/okuuva/auto-save.nvim/tree/main
+vim.api.nvim_create_autocmd(
+  { 'BufLeave', 'FocusLost', 'InsertLeave', 'TextChanged' },
+  {
+    callback = function(opts)
+      if vim.fn.getbufvar(opts.buf, '&modifiable') == 1 then
+        vim.cmd("silent! update")
+      end
+    end,
+    desc = 'Save current buffer automatically'
+  })
