d27623569c89fdfb32e434fdadf07aa248ba8b24
Mario D'Andrea <mdandrea@disroot.org>
2024-04-22 13:07:15 +0200

    Refactor of scr.sh complete

--- .local/bin/scr.sh
+++ .local/bin/scr.sh
@@ -9,56 +9,82 @@
 # Ported later to sh and sway by Mario D'Andrea <https://ormai.dev>
 
 SCRIPT=$(basename "$0")
-NOTIFY=false
-CURSOR=false # whether to include the mouse pointer in the screenshot
 
 usage() {
-  echo "Usage:
-  $SCRIPT [-n|-c] [copy|save|copysave] [active|screen|output|area] [FILE|-]
-
-Commands:
-  copy: Copy the screenshot data into the clipboard.
-  save: Save the screenshot to a regular file or '-' to pipe to STDOUT.
-  copysave: Combine the previous 2 options.
-  usage: Show this message and exit.
-
-Targets:
-  active: Currently active window.
-  screen: All visible outputs.
-  output: Currently active output.
-  area: Manually select a region or window."
-  exit 0
-}
-
-# parse options
-while [ $# -gt 0 ]
-do
-  case "$1" in
-  -c)
-    CURSOR=true
-    shift
-    ;;
-  -h) usage ;;
-  -n)
-    NOTIFY=true
-    shift
-    ;;
-  *) break ;; # unknown option, done with parsing
-  esac
-done
-
-notify() {
-  $NOTIFY && notify-send -u low -t 3000 -a "$SCRIPT" "$@" || echo "$@"
-}
-
-fatal() {
-  msg=${1:-Error taking screenshot with "$SCRIPT"}
-  $NOTIFY && notify-send -u critical -t 5000 -a "$SCRIPT" "$msg" || echo "$msg"
-  exit 2
+  echo "$SCRIPT: take screenshots on sway using grim"
+  echo "Usage: $SCRIPT [-chnms] [-f file] [-t target]"
+  echo
+  echo "Options:"
+  echo "  -c         Copy screenshot to clipboard"
+  echo "  -f file    Specify file to save the screenshot"
+  echo "  -h         Show this help message"
+  echo "  -n         Enable desktop notifications"
+  echo "  -m         Include mouse pointer in the screenshot"
+  echo "  -s         Save screenshot to file"
+  echo "  -t target  Specify target of the screenshot"
+  echo
+  echo "Targets:"
+  echo "  active     The currently focused container"
+  echo "  area       Select container, output or custom area"
+  echo "  output     The currently focused output"
+  echo "  screen     All visible outputs"
 }
 
 check_dependency() {
   command -v "$1" > /dev/null || fatal "$1 is missing"
+}
+
+NOTIFY=false
+TARGET=screen
+
+while getopts cf:hnmst: option
+do
+  case "$option" in
+    c)
+      check_dependency wl-copy
+      COPY=true
+      ;;
+    f) FILE="$OPTARG" && SAVE=true ;;
+    h) usage && exit ;;
+    n) NOTIFY=true ;;
+    m) MOUSE=true ;;
+    s) SAVE=true ;;
+    t) TARGET="$OPTARG" ;;
+    \?) exit 1 ;;
+  esac
+done
+
+fatal() {
+  msg=${1:-Error taking screenshot with "$SCRIPT"}
+  if $NOTIFY
+  then
+    notify-send -u critical -t 5000 -a "$SCRIPT" "$msg"
+  else
+    printf "\033[31merror\033[0m: %s\n" "$msg"
+  fi
+  exit 1
+}
+
+screenshot_directory() {
+  . "${XDG_CONFIG_HOME:-$HOME/.config}/user-dirs.dirs" 2> /dev/null
+  echo "${XDG_SCREENSHOTS_DIR:-${XDG_PICTURES_DIR:-$HOME}}"
+}
+
+[ "$COPY" ] && ACTION=copy
+[ "$SAVE" ] && ACTION=${ACTION}save
+[ -z "$ACTION" ] && ACTION=save
+if [ -n "${ACTION#copy}" ]
+then
+  FILE=${FILE:-$(screenshot_directory)/$(date +%FT%H-%M-%S%z).png} # ISO 8601
+fi
+
+notify() {
+  if $NOTIFY
+  then
+    notify-send ${SAVE:+-i $FILE} -u low -t 3000 -a "$SCRIPT" "$@"
+  else
+    echo "$SCRIPT:" "$@"
+  fi
 }
 
 # If any of these is missing there is no point in continuing
@@ -67,68 +93,42 @@ do
   check_dependency "$cmd"
 done
 
-screenshot_directory() {
-  . "${XDG_CONFIG_HOME:-$HOME/.config}/user-dirs.dirs" 2> /dev/null
-  echo "${XDG_SCREENSHOTS_DIR:-${XDG_PICTURES_DIR:-$HOME}}"
-}
-
-ACTION=${1:-save}
-TARGET=${2:-screen}
-FILE=${3:-$(screenshot_directory)/$(date +%FT%H-%M-%S%z).png} # ISO 8601 format
-
-take_screenshot() {
-  if [ -n "$3" ]
-  then
-    grim ${CURSOR:+-c} -o "$3" "$1"
-  elif [ -z "$2" ]
-  then
-    grim ${CURSOR:+-c} "$1"
-  else
-    grim ${CURSOR:+-c} -g "$2" "$1"
-  fi
-}
-
 case "$TARGET" in
   active)
-    FOCUSED=$(swaymsg -t get_tree | jq -r 'recurse(.nodes[]?, .floating_nodes[]?) | select(.focused)')
-    GEOMETRY=$(echo "$FOCUSED" | jq -r '.rect | "\(.x),\(.y) \(.width)x\(.height)"')
-    APP_ID=$(echo "$FOCUSED" | jq -r '.app_id')
-    WHAT="$APP_ID window"
-    ;;
-  screen)
-    GEOMETRY=""
-    WHAT="Screen"
-    ;;
-  output)
-    GEOMETRY=""
-    OUTPUT=$(swaymsg -t get_outputs | jq -r '.[] | select(.focused)' | jq -r '.name')
-    WHAT="$OUTPUT"
+    focused=$(swaymsg -t get_tree \
+      | jq -r "recurse(.nodes[]?, .floating_nodes[]?) | select(.focused)")
+    GEOMETRY=$(echo "$focused" \
+      | jq -r '.rect | "\(.x),\(.y) \(.width)x\(.height)"')
+    TARGET="$(echo "$focused" | jq -r '.app_id') window"
     ;;
   area)
     check_dependency slurp
-    GEOMETRY=$(swaymsg -t get_tree | jq -r '.. | select(.pid? and .visible?) | .rect | "\(.x),\(.y) \(.width)x\(.height)"' | slurp -o)
-    [ -z "$GEOMETRY" ] && exit 0 # user did not select area
-    WHAT="Area"
+    GEOMETRY=$(swaymsg -t get_tree \
+      | jq -r '.. | select(.pid? and .visible?) | .rect | "\(.x),\(.y) \(.width)x\(.height)"' \
+      | slurp -o)
+    [ -z "$GEOMETRY" ] && exit # user did not select anything
     ;;
+  output)
+    OUTPUT=$(swaymsg -t get_outputs | jq -r '.[] | select(.focused)' \
+      | jq -r '.name')
+    ;;
+  screen) ;;
+  *) fatal "Unknown target '$TARGET'" ;;
 esac
 
 case "$ACTION" in
   save)
-    if take_screenshot "$FILE" "$GEOMETRY" "$OUTPUT"
-    then
-      notify -i "$FILE" "Screenshot of $TARGET" "$(basename "$FILE")"
-    else
-      fatal
-    fi
+    grim ${MOUSE:+-c} ${OUTPUT:+-o $OUTPUT} ${GEOMETRY:+-g "$GEOMETRY"} "$FILE"
+    notify "$TARGET saved to $FILE"
     ;;
   copy)
-    check_dependency wl-copy
-    take_screenshot - "$GEOMETRY" "$OUTPUT" | wl-copy --type image/png
-    notify "$WHAT copied to clipboard"
+    grim ${MOUSE:+-c} ${OUTPUT:+-o $OUTPUT} ${GEOMETRY:+-g "$GEOMETRY"} - \
+      | wl-copy -t image/png
+    notify "$TARGET copied to clipboard"
     ;;
   copysave)
-    check_dependency wl-copy
-    take_screenshot - "$GEOMETRY" "$OUTPUT" | tee "$FILE" | wl-copy --type image/png
-    notify -i "$FILE" "$WHAT" "Copied to clipboard and saved to $FILE"
+    grim ${MOUSE:+-c} ${OUTPUT:+-o $OUTPUT} ${GEOMETRY:+-g "$GEOMETRY"} - \
+      | tee "$FILE" | wl-copy -t image/png
+    notify "$TARGET copied to clipboard and saved to $FILE"
     ;;
 esac
