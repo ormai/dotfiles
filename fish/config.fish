73edf5f024426b6750ea5eaecc4ceb264dd6bc01
Mario D'Andrea <goffredo2004@gmail.com>
2024-12-11 11:21:27 +0100

    Add fastfetch config

--- fish/config.fish
+++ fish/config.fish
@@ -1,4 +1,9 @@
 if status is-login
-
   source $HOME/.config/fish/env.fish
+end
+
+if status is-interactive
+  abbr s git status --short
+  abbr d git diff
+  abbr l git log --oneline --decorate --graph
 end
