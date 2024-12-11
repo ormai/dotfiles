b01a14a6c2f8600cc3699929bde746ceebd5e519
Mario D'Andrea <goffredo2004@gmail.com>
2024-12-11 11:12:29 +0100

    chsh -s /usr/bin/fish
    Migrating all environment variables to fish

--- fish/functions/ll.fish
+++ fish/functions/ll.fish
@@ -1,3 +1,3 @@
-function ll --wraps='eza -Bla --sort=modified --git --time-style=long-iso' --description 'alias ll eza -Bla --sort=modified --git --time-style=long-iso'
+function ll --wraps='eza -Bla --sort=modified --git --time-style=long-iso' --description 'List files with details'
     eza -Bla --sort=modified --git --time-style=long-iso $argv
 end
