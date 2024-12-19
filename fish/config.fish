fdad037c7fa68e951b77865c8ccc931f624d4528
Mario D'Andrea <goffredo2004@gmail.com>
2024-12-19 09:52:35 +0100

    fish: add abbreviations and change some environment variables

--- fish/config.fish
+++ fish/config.fish
@@ -3,7 +3,22 @@ if status is-login
 end
 
 if status is-interactive
+  if test $TERM = foot
+    # Jumping between prompts
+    function mark_prompt_start --on-event fish_prompt
+      echo -en "\e]133;A\e\\"
+    end
+  end
+
   abbr s git status --short
   abbr d git diff
   abbr l git log --oneline --decorate --graph
+
+  abbr --position anywhere ip ip -c
+  abbr mv mv -iv
+  abbr rm rm -i
+  abbr cp cp -i
+  abbr trash gio trash
+
+  abbr adb HOME=$XDG_DATA_HOME/android adb
 end
--- fish/functions/clean.fish
+++ fish/functions/clean.fish
@@ -3,5 +3,5 @@ function clean
   sudo paccache -r
   paru -Sccd --noconfirm
   pip cache purge
-  rm -rf $XDG_DATA_HOME/Trash/files/*
+  gio trash --empty
 end
