29214d44ecc8f8e919e3249ad674cf5735e1dc4c
Mario D'Andrea <goffredo2004@gmail.com>
2024-12-20 18:18:34 +0100

    nvim: scratch terminal now updates on VimResize

--- nvim/lua/scratch-terminal.lua
+++ nvim/lua/scratch-terminal.lua
@@ -1,27 +1,39 @@
 -- https://youtu.be/5PIiKDES_wc
 
-local scratch_term = { win = -1, buf = -1 }
+local self = { win = -1, buf = -1 }
+
+local function config()
+  local c = {
+    relative = 'editor',
+    style = 'minimal',
+    width = math.floor(vim.o.columns * (vim.o.columns < 150 and 0.9 or 0.7)),
+    height = math.floor(vim.o.lines * 0.8),
+    border = 'solid',
+  }
+  c.col = (vim.o.columns - c.width - 2) / 2
+  c.row = (vim.o.lines - c.height - 2) / 2
+  return c
+end
+
+vim.api.nvim_create_autocmd('VimResized', {
+  group = vim.api.nvim_create_augroup('scratch-terminal', { clear = true }),
+  callback = function()
+    if vim.api.nvim_win_is_valid(self.win) then
+      vim.api.nvim_win_set_config(self.win, config())
+    end
+  end
+})
 
 Keymap('<C-BS>', function()
-  if not vim.api.nvim_win_is_valid(scratch_term.win) then
-    if not vim.api.nvim_buf_is_valid(scratch_term.buf) then
-      scratch_term.buf = vim.api.nvim_create_buf(false, true)
+  if not vim.api.nvim_win_is_valid(self.win) then
+    if not vim.api.nvim_buf_is_valid(self.buf) then
+      self.buf = vim.api.nvim_create_buf(false, true)
     end
-    local width = math.floor(vim.o.columns * 0.8)
-    local height = math.floor(vim.o.lines * 0.8)
-    scratch_term.win = vim.api.nvim_open_win(scratch_term.buf, true, {
-      relative = 'editor',
-      style = 'minimal',
-      width = width,
-      height = height,
-      col = math.floor((vim.o.columns - width) / 2),
-      row = math.floor((vim.o.lines - height) / 2),
-      border = 'solid'
-    })
-    if vim.bo[scratch_term.buf].buftype ~= 'terminal' then
+    self.win = vim.api.nvim_open_win(self.buf, true, config())
+    if vim.bo[self.buf].buftype ~= 'terminal' then
       vim.cmd.terminal()
     end
   else
-    vim.api.nvim_win_hide(scratch_term.win)
+    vim.api.nvim_win_hide(self.win)
   end
 end, 'Toggle scratch terminal', { 'n', 't', 'i', 'x' })
