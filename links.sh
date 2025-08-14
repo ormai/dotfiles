ad2617fe5258fa26ea834117db8e9dca9c2349b4
Mario D'Andrea <goffredo2004@gmail.com>
2025-08-14 18:29:40 +0200

    update file inclusions

--- links.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-# Here I map the location of files external to ~/.config
-
-ln -s ~/.config/firefox/chrome ~/.mozilla/mario/chrome
-ln -s ~/.config/firefox/user-overrides.js ~/.mozilla/mario/user-overrides.js
