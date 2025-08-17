e60279ccbe6a25daa1c42b2a1c033a58cf0cdeb1
Mario D'Andrea <goffredo2004@gmail.com>
2025-08-17 16:28:16 +0200

    update paru wrapper

--- fish/functions/paru.fish
+++ fish/functions/paru.fish
@@ -1,6 +1,6 @@
 function paru -w paru
-  # Update the package list after using paru
-  command paru $argv
-    and command paru -Qqe | grep -vxf (command paru -Qqm | psub) >$XDG_CONFIG_HOME/packages.txt
-    and command paru -Qqm >$XDG_CONFIG_HOME/packages-aur.txt
+    # Update the package list after using paru
+    command paru $argv
+    and command paru -Qqm > $XDG_CONFIG_HOME/packages-aur.txt
+    and command paru -Qqe | grep -vxf $XDG_CONFIG_HOME/packages-aur.txt > $XDG_CONFIG_HOME/packages.txt
 end
