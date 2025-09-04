9170d88b00ff96a58e3d07f82da94fe05e42eabb
Mario D'Andrea <goffredo2004@gmail.com>
2025-09-04 10:53:45 +0200

    chore(nvim): Lazy sync

--- nvim/lua/plugins/init.lua
+++ nvim/lua/plugins/init.lua
@@ -12,4 +12,12 @@ return {
     end
   },
   { "OXY2DEV/ui.nvim" },
+  {
+    'oribarilan/lensline.nvim',
+    branch = 'release/1.x',
+    event = 'LspAttach',
+    config = function()
+      require("lensline").setup()
+    end,
+  }
 }
