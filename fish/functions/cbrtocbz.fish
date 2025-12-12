b6e8856028ff1aff5e3c6bd4290b7e4c78880e20
Mario D'Andrea <goffredo2004@gmail.com>
2025-12-12 14:12:49 +0100

    make cbrtocbz.fish sturdier

--- fish/functions/cbrtocbz.fish
+++ fish/functions/cbrtocbz.fish
@@ -1,15 +1,23 @@
-function cbrtocbz --description 'Convert a comic book from RAR to ZIP'
+function cbrtocbz -d 'Convert a comic book archive from RAR to ZIP'
     if test (count $argv) -lt 1
         echo "Usage: $_ files..."
         return 1
     end
 
+    # Extracts the .cbr with unrar, then creates a .cbz with zip
+    function convert -a file
+        set -l workdir (mktemp -d)
+        and unrar e $file -o $workdir
+        and zip (path change-extension 'cbz' $file) $workdir/*
+        and gio trash $file # Original file is not lost 
+        and rm -r $workdir
+    end
+
     for file in $argv
-        # unrar the .cbr, then create a .cbz with zip
-        set -l name (path change-extension '' $file)
-        mkdir -p .$name
-        and unrar e $file -o .$name
-        and zip $name.cbz .$name/*
-        and gio trash .$name $file
+        if test -f $file && test (path extension $file) = .cbr
+            convert $file
+        else
+            echo "[WARN] skipping '$file': it is not a valid comic book RAR archive"
+        end
     end
 end
