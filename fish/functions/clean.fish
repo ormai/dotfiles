9f8eec5133664a0404ce4c9246ffe0ef4cff0cb0
Mario D'Andrea <goffredo2004@gmail.com>
2026-07-08 10:24:22 +0200

    Deeper cleaning

--- fish/functions/clean.fish
+++ fish/functions/clean.fish
@@ -2,10 +2,13 @@ function clean --description 'Free storage'
     paru -Rsnu (paru -Qqd) # orphans
     sudo paccache -rk1
     sudo paccache -ruk0
-    paru --noconfirm -Sccd
+    yes | paru -Sccd
     sudo docker system prune --all --force
     # cargo cache --autoclean
     pip cache purge
     uv cache clean
     gio trash --empty
+    sudo journalctl --vacuum-time=2d
+    npm cache clean --force
+    npm cache verify
 end
