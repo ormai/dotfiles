44442255e8e52c13e554ddd2f079b74571867317
Mario D'Andrea <goffredo2004@gmail.com>
2026-07-08 10:23:04 +0200

    Add adb alias

--- fish/abbreviations.fish
+++ fish/abbreviations.fish
@@ -1,5 +1,5 @@
-abbr ls 'eza --hyperlink'
-abbr ll 'eza -laM -s modified --git --time-style=iso --icons --hyperlink'
+abbr ls eza
+abbr ll 'eza -laM -s modified --icons --git --time-style=iso'
 abbr lt 'eza -T --icons --hyperlink'
 abbr mv 'mv -iv'
 abbr rm 'rm -iv'
@@ -17,10 +17,10 @@ abbr venv 'source .venv/bin/activate.fish'
 abbr onefetch 'onefetch --number-of-authors 5 --number-of-file-churns 0 --no-title'
 abbr monero 'monero-wallet-cli --log-file $XDG_STATE_HOME/monero/monero-wallet-cli.log --wallet-file $XDG_DOCUMENTS_DIR/MoneroWallet --daemon-address 192.168.1.8:18089 --trusted-daemon'
 abbr irssi 'irssi --config=$XDG_CONFIG_HOME/irssi/config.conf --home=$XDG_DATA_HOME/irssi'
+alias adb='HOME="$XDG_DATA_HOME"/android adb'
 
 # Git
-abbr a git add
-abbr s git status --short
-abbr d git diff
-abbr c git commit -v
-abbr l git log --graph --abbrev-commit --date=relative --pretty=format:"'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr)%Creset'"
+abbr a 'git add'
+abbr s 'git status --short'
+abbr d 'git diff'
+abbr c 'git commit -v'
